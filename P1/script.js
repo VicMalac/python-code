c62d5ece11128f1a5d0e3d5350171d3352de7c7e
Victor <102198944+VicMalac@users.noreply.github.com>
2025-03-12 00:48:18 -0300

    Delete P1 directory

--- P1/script.js
+++
@@ -1,8 +0,0 @@
-const altern = document.querySelector('.btn');
-
-altern.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
-    document.body.classList.toggle('light-theme')
-    
-
-});
