c7965719571ab3c80698dd7ed230b07141fd4713
Victor <victorpdpyt9098@gmail.com>
2025-03-14 22:57:30 -0300

    Update aula1.py

--- secao1/aula1.py
+++ secao1/aula1.py
@@ -1,7 +1,16 @@
 """
+<<<<<<< Updated upstream:secao1/aula1.py
      o interpretador do Python lê essas 3 aspas
      além de que pode ter diversas linhasss
+=======
+     O interpretador do Python lê essas 3 aspas
+     Além de que pode ter diversas linhas
+     Isso aqui é uma docstring
+>>>>>>> Stashed changes:aula1.py
 """
 #Diferente do hashtag que não é lido
 
-print('123')
+print("Amei meu novo LED, ficou muito bonito", "\nTestando quebra de linha")
+
+print(32, 78, sep= "-", end="\n")
+print(21, 21, sep= "-", end="\n")
