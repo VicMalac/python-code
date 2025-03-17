5861dbdc3f8f8f645c024956a7e610a530c9899b
VicMalac <victorpdpyt9098@gmail.com>
2025-03-17 13:10:26 -0300

    Create aula40.py

---
+++ secao1/aula40.py
@@ -0,0 +1,28 @@
+# Calculadora com while
+
+while True:
+    num1 = input("Digite o primeiro número: ")
+    num2 = input("Digite o segundo número: ")
+    opr = input("Digite o operador Ex:\n[+] Adição\n[-] Subtração\n[*] Multiplicação\n[/] Divisão\n[**]Potenciação\n[//]Divisão Inteira\n[%] Módulo\n: ")
+
+
+    if opr == '+': # Adição
+            print(f'{num1} + {num2} = {float(num1) + float(num2)}')
+    if opr == '-': # Subtração
+            print(f'{num1} - {num2} = {float(num1) - float(num2)}')
+    if opr == '*': # Multiplicação
+            print(f'{num1} * {num2} = {float(num1) * float(num2)}')
+    if opr == '/': # Divisão
+            print(f'{num1} / {num2} = {float(num1) / float(num2)}')
+    if opr == '**': # Potenciação
+            print(f'{num1} ** {num2} = {float(num1) ** float(num2)}')
+    if opr == '//': # Divisão inteira
+            print(f'{num1} // {num2} = {float(num1) // float(num2)}')
+    if opr == '%': # Modulo
+        print(f'{num1} % {num2} = {float(num1) % float(num2):.1f}')
+
+
+
+    sair = input("Deseja sair? [s]im ou [n]ão: ").lower().startswith('s')
+    if sair:
+        break
