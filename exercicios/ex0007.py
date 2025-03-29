ff22c3d0b950fcdf98ff56cb78d250cd033d9dcc
Victor <victorpdpyt9098@gmail.com>
2025-03-29 01:53:54 -0300

    Update
    Atualizando o exercício da agenda

--- exercicios/ex0007.py
+++ exercicios/ex0007.py
@@ -3,10 +3,12 @@ Agenda de Contatos:
 
 Objetivo: Implementar um programa que permite ao usuário adicionar, remover, buscar e listar contatos. Cada contato deve conter nome, telefone e e-mail.
 """
-nomes = []
-telefones = []
-num = 0
+nomes = ['victor', 'victor', '123', 'rato']
+telefones = ['123', '3321', '123', '4321']
+print(telefones[1])
+
 while True:
+        num = 0
         parcont = int(input("[1] Adicionar Contato\n[2] Remover Contato\n[3] Ver Contatos\n[4] Buscar Contato\n[5] Encerrar Agenda\nEscolha uma opção: "))
         #system("cls")
         match parcont:
@@ -17,23 +19,52 @@ while True:
                 telefones.append(telefone)
                 print(nomes[len(nomes) - 1], telefone)
                 continue
+
+
+
+
             case 2: # Remover Contatos (Em Processo)
-                remove = input("Qual contato deseja remover: ")
-                if nomes.count(remove) >= 2:
-                    #remove = input("Existem contatos com informações iguais, qual deseja remover: ")
+                remover = input("Qual contato deseja remover: ")
+                if nomes.count(remover) >= 2:
                     for nome in nomes: # Nome é uma str, então não dá para usar como contador
-                        print(num, '-', nome)
+                        print(f'nome[{num + 1}] - {nome}')
                         num += 1
-                elif telefones.count(remove) >= 2:
-                    ...
+                    remove = input("Existem contatos com nomes iguais, qual deseja remover: ")
+
+                elif telefones.count(remover) >= 2:
+                    for telefone in telefones:
+                        print(f'tel[{num + 1}] - {telefone}')
+                        num += 1
+                    remove = input("Existem contatos com telefones iguais, qual deseja remover: ")
+
+                else:
+                    varn = nomes.index(remover) # 3
+
+                    print(f"Indice: {varn}")
+
+                    if remover in telefones:
+                        telefones.remove(remover)
+                        nomes.remove(telefones[varn])
+                        print('telefone', telefones)
+                        print('nome', telefones)
+                    else:
+                        nomes.remove(remover)
+                        telefones.remove(telefones[varn])
+                        print('telefone', telefones)
+                        print('nome', telefones)
+
                 continue
-            case 3: # Ver Contatos (Em Processo)
+
+
+
+
+            case 3: # Ver Contatos (Funcionando)
                 for i in range(len(nomes)):
-                    print(f'{nomes[i]}, {telefones[i]}')
+                    print(f'{nomes[i]}, {telefones[i - 1]}')
                 continue
             case 4: # Buscar Contatos (Em Processo)
                 continue
-            case 5: # Encerrar Agenda (Em Processo)
+            case 5: # Encerrar Agenda (Funcionando)
                 print("Obrigado por utilizar a agenda! :)")
                 break
             case _:
---
+++ teste.py
@@ -0,0 +1,6 @@
+lista = ['victor', 'teste', '123', 'macaco']
+vic = 'Victor'
+vixk = 'victor'
+true = vixk in lista
+var = lista.index("123")
+print(true)
