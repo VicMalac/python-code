d2e88c9d845018f96d9e4d490fb5b327fc591a44
Victor <victorpdpyt9098@gmail.com>
2025-04-04 23:23:02 -0300

    Update ex009.py

--- exercicios/ex009.py
+++ exercicios/ex009.py
@@ -1,7 +1,15 @@
 # Mostre os primeiros n números da sequência de Fibonacci.
+# 1 1 2 3 5 8 13 21 34 55 89 144 233
 
 num = int(input('Digite um número: '))
+x = 1
+y = 1
 
-fibonacci = 0
-for i in range(num):
-    ...
+for i in range(1, num, 2):
+    print(f'{x}', end = ' ')
+    print(f'{y}', end = ' ')
+    x += y
+    y += x
+
+if num % 2 != 0:
+    print(f'{x}', end = ' ')
