03d40585f0682299b6dc683bb0fc8b13b51cf976
Victor <victorpdpyt9098@gmail.com>
2025-04-12 23:23:38 -0300

    Create ex012.py

---
+++ exercicios/ex012.py
@@ -0,0 +1,15 @@
+# Trocar valor de duas variaveis sem usar uma terceira
+
+a = 21
+b = 9
+c = 6
+
+print(f'A: {a}\nB: {b} \nC: {c}\n')
+b = b - a
+a = b + a
+b = a - b
+c = c - a
+a = c + a
+c = a - c
+
+print(f'A: {a}\nB: {b} \nC: {c}')
