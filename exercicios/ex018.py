a3b3036b7209e3ba4bc3e611a479b0d9783eab9f
VicMalac <victorpdpyt9098@gmail.com>
2025-05-27 12:03:31 -0300

    Funções

---
+++ exercicios/ex018.py
@@ -0,0 +1,30 @@
+# Usar Funções
+
+# def Mensagem():
+#     print("Hello World!")
+
+# def Msg():
+#     return "Hello World"
+
+# Mensagem()
+
+# texto = Msg()
+# print (texto)
+
+# def AcharNota():
+#     nota = int(input("Digite uma nota: "))
+#     return nota
+
+# def Resultado(nota1, nota2):
+#     media = (nota1 + nota2) / 2
+#     if media >= 6:
+#         print("Aprovado!")
+#     elif media < 6 and media >= 4:
+#         print("Recuperação!")
+#     else:
+#         print("Reprovado!")
+
+# a = AcharNota()
+# b = AcharNota()
+# Resultado(a, b)
+
